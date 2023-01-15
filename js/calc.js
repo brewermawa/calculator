f35ccc4eae15f7ce177f012244c8bd795c1cd61a
Mawa Guerra <brewermawa@gmail.com>
2023-01-15 11:20:14 -0600

    Basic operations done, now I have to work with negative results

--- js/calc.js
+++ js/calc.js
@@ -1,8 +1,32 @@
-addEvents();
+addEventsToNumbers();
 let screen = document.querySelector("#screen");
+let clear = document.querySelector("#clear")
+let sum = document.querySelector("#sum");
+let minus = document.querySelector("#minus");
+let multiplication = document.querySelector("#multiplication");
+let division = document.querySelector("#division");
+let equals = document.querySelector("#equals");
+let operand1 = null;
+let operand2 = null;
+let operation = null;
+let lastButtonClicked = null;
+addEventsToSigns();
 dotEntered = false;
 
-function addEvents() {
+clear.addEventListener("click", () => {
+    screen.textContent = "0";
+    dotEntered = false;
+});
+
+function addEventsToSigns() {
+    sum.addEventListener("click", sumClicked);
+    minus.addEventListener("click", minusClicked);
+    multiplication.addEventListener("click", multiplicationClicked);
+    division.addEventListener("click", divisionClicked);
+    equals.addEventListener("click", equalsClicked);
+}
+
+function addEventsToNumbers() {
     let numbers = document.querySelectorAll(".number");
     
     numbers.forEach((e) => {
@@ -13,10 +37,17 @@ function addEvents() {
 };
 
 function addNumberToScreen(id) {
+    if (lastButtonClicked === "sum" || 
+        lastButtonClicked === "equals" || 
+        lastButtonClicked === "minus" ||
+        lastButtonClicked === "multiplication" ||
+        lastButtonClicked === "division") {
+        screen.textContent = "";        
+    }
+
     if (screen.textContent.length < 10) {
         if (screen.textContent === "0") {
-            screen.textContent = id;
-            console.log("zero");
+            id === "dot" ? screen.textContent += "." : screen.textContent = id;
         }
         else {
             if (id === "dot") {
@@ -29,5 +60,93 @@ function addNumberToScreen(id) {
                 screen.textContent += id;
             }
         }
+        lastButtonClicked = id;
+    }
+}
+
+function sumClicked() {
+    if (!operand1) {
+        operand1 = parseFloat(screen.textContent);
+    }
+    else {
+        operand2 = parseFloat(screen.textContent);
+        operand1 = operate();
+        operand2 = null;
+    }
+
+    operation = "sum";
+    dotEntered = false;
+    lastButtonClicked = "sum";
+};
+
+function minusClicked() {
+    if (!operand1) {
+        operand1 = parseFloat(screen.textContent);
+    }
+    else {
+        operand2 = parseFloat(screen.textContent);
+        operand1 = operate();
+        operand2 = null;
+    }
+
+    operation = "minus";
+    dotEntered = false;
+    lastButtonClicked = "minus";
+};
+
+function multiplicationClicked() {
+    if (!operand1) {
+        operand1 = parseFloat(screen.textContent);
+    }
+    else {
+        operand2 = parseFloat(screen.textContent);
+        operand1 = operate();
+        operand2 = null;
+    }
+
+    operation = "multiplication";
+    dotEntered = false;
+    lastButtonClicked = "multiplication";
+};
+
+function divisionClicked() {
+    if (!operand1) {
+        operand1 = parseFloat(screen.textContent);
+    }
+    else {
+        operand2 = parseFloat(screen.textContent);
+        operand1 = operate();
+        operand2 = null;
+    }
+
+    operation = "division";
+    dotEntered = false;
+    lastButtonClicked = "division";
+};
+
+function equalsClicked() {
+    operand2 = parseFloat(screen.textContent);
+    dotEntered = false;
+    lastButtonClicked = "equals";
+    operate();
+    operand1 = null;
+    operand2 = null;
+    operation = null;
+}
+
+function operate() {
+    switch (operation) {
+        case "sum":
+            screen.textContent = operand1 + operand2;
+            return operand1 + operand2;
+        case "minus":
+            screen.textContent = operand1 - operand2;
+            return operand1 - operand2;
+        case "multiplication":
+            screen.textContent = operand1 * operand2;
+            return operand1 * operand2;
+        case "division":
+            screen.textContent = operand1 / operand2;
+            return operand1 / operand2;
     }
 }
